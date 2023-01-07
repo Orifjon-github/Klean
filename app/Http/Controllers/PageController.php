2529b30fb1eb79c02221b4d8c55cf0a04338860e
Orifjon94 <avazbekoripov94@gmail.com>
2023-01-08 01:18:33 +0500

    leran blade

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -9,4 +9,13 @@ class PageController extends Controller
     public function index() {
         return view('main');
     }
+
+    public function welcome()
+    {
+        return view('welcome', [
+            "name" => "John",
+            "records" => [1, 2, 3]
+        
+        ]);
+    }
 }
